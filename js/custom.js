717f42baba7b44c376c1db8ee67cfd35b4983864
Charlie Goodson <max.sadchikov@gmail.com>
2018-01-01 21:07:59 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -40,10 +40,20 @@ $(document).ready(function(){
 		});
 	}
 
-	// добавление галлереи
+	// добавление раздела галлереи
 	$('#view_all_gallery').click(function() {
 		let content = '<div class="gallery_line main_flex__nowrap flex__jcontent_between"><div class="box_img_gallery"><img src="img/gallery/thumbnail-01.jpg" alt=""></div><div class="box_img_gallery"><img src="img/gallery/thumbnail-02.jpg" alt=""></div></div><div class="name_gallery"><p>Marina Palms / <span class="blue">North Miami Beach, FL 33162</span></p></div>';
 		$('#gallery_box').append(content);
 		$(this).remove();
 	});
+
+	// показ формы заказа звонка
+	$('#modal_box, #black_fill').hide();
+	$('#call').click(function() {
+		$('#modal_box, #black_fill').show();
+	});
+	$('#close_modal, #black_fill').click(function() {
+		$('#modal_box, #black_fill').hide();
+	});
+
 });
